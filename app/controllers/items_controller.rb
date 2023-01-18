d78daa61b47a3d33ca492365363cf819ee52f149
aya05820 <gsbdkcg5835@gmail.com>
2023-01-18 21:03:26 +0900

    商品一覧表示機能　訂正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @item_infos = ItemInfo.all
+    @item_infos = ItemInfo.includes(:user).order("created_at DESC")
   end
 
   def new
