c847d7c72370046b4fefa225f7372f01ae0f3a5c
aya05820 <gsbdkcg5835@gmail.com>
2023-01-19 20:50:53 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   
   def index
@@ -35,6 +35,14 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+   if @item_info.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
 
